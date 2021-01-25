fa077039ca91fa4fcef07984082ac47c64ae6740
cong85010 <58084805+cong85010@users.noreply.github.com>
2021-01-25 10:18:44 +0700

    Add files via upload

--- web-banHang/js/main.js
+++ web-banHang/js/main.js
@@ -1,3 +1,23 @@
-function timkiem(){
-    
+var checkSearch = false;
+
+function search_out(){
+    if(checkSearch == false) {
+        document.getElementById('input_bar').classList.add('timra')
+        change_search()
+        checkSearch = true;
+    }
+    else {
+        closetim()
+        document.getElementById('button-addon').innerHTML = '<i class="fa fa-search"></i>'
+    }
+}
+function closetim() {
+    if(checkSearch == true) {
+        document.getElementById('input_bar').classList.remove('timra')
+        checkSearch = false
+    }
+}
+function change_search (){
+    document.getElementById('button-addon').innerHTML = '<i class="fas fa-times-circle"></i>'
+    checkSearch = false
 }
